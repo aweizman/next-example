c7d65a393831b58dd2ec403324a8061e1e341ff2
John Cyr <jcyr@add123.com>
2022-11-11 10:13:06 -0500

    First Commit

---
+++ components/fields/TextField.spec.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from '@jest/globals'
+import TextField from './TextFields'
+
+describe('TextField', () => {
+  it('should exist', () => {
+    expect(<TextField />).toBeDefined()
+  })
+})
